9f241654e3d37f2726949efb6b947eabc8433018
MonikaNova <97058693+MonikaNova@users.noreply.github.com>
2022-01-03 20:31:24 +0100

    Add files via upload

---
+++ Calculator app/js/advanced.js
@@ -0,0 +1,123 @@
+"use strict";
+let someVal = ["jojo", "lolo", "koko", "momo"];
+someVal.forEach((val) => {
+    console.log(val);
+});
+// let prompt1 = prompt("Insert name:")
+// let myArray: string []=  []
+// let index: number = 3
+// let newArray: string[] = []
+// //override element
+// let arrayTest:(string | number | boolean)[] = ["test1",2,true,"456"]
+// arrayTest[0] = 123
+// console.log(arrayTest)
+// //add the last element
+// let myArray: string[] = ["one","two","three"]
+// myArray.push("kokote")
+// console.log(myArray)
+// //remove the last element 
+// myArray.pop()
+// console.log(myArray)
+// //add the first element
+// myArray.unshift("This the first element of the array")
+// console.log(myArray)
+// //remove the first element 
+// myArray.shift()
+// console.log(myArray)
+// //remove of any position in the array
+// let splicedArray = ['One','Two','Three','Four']
+// splicedArray.splice(3,4)
+// splicedArray.splice(4,0,"K")
+// console.log(splicedArray)
+// let password: string[] = ["123","7896","5632","administratorG1neral01","lolor456"]
+// let res: string = ""
+// let randomNum: number = Math.ceil(Math.random() * 5)
+// let index: number = password.length - randomNum
+// for(let i = 0;i < password.length;i++) {
+//        res =  password[index]
+// }
+// console.log(res)
+// let number1: number = Math.ceil(Math.random() * 6)
+// let number2: number = Math.ceil(Math.random() * 6)
+// let number3: number = Math.ceil(Math.random() *6)
+// let number4: number = Math.ceil(Math.random() *6)
+// let number5: number = Math.ceil(Math.random() *6)
+// let number6: number = Math.ceil(Math.random() *6)
+// let numbers: number[] = [number1,number2,number3,number4,number5,number6]
+// let array: number[] = [1 , 2,5,8,97,10,456]
+//   let output: number = 0
+// function sum(): number {
+//     for(let i = 0;i < numbers.length;i++) {
+//         output += numbers[i]       
+//     }
+//      return output
+// }
+// console.log(sum())
+// function lastElementInArray() {
+//     for(let i = 0;i < array.length;i++) {
+//         output = array[i]
+//     }
+//     console.log("This is the last element of the array " + output)
+// }
+// lastElementInArray()
+// function caller(s: number, b: boolean): string {
+//    if(output >= 25) {
+//        return "You are winner"
+//    }else {
+//        return "try your luck one more"
+//    }
+// }
+// let f: string =  caller(output,false)
+// console.log(f)
+// let number: number = 3.2568
+// let fixed: string = number.toFixed()
+// console.log(fixed)
+// console.log(Math.round(Math.random() * 100))
+// console.log(Math.round(Math.random() * 10))
+// console.log(Math.round(Math.random() * 10))
+// console.log(Math.round(Math.random() * 10))
+// console.log(Math.round(Math.random() * 10))
+// let min = 1
+// let max = 20
+// const interval = Math.floor(Math.random() * (max- min + 1)) + min
+// console.log("Interval between 15 to 20 is: " + interval)
+// console.log("Interval between 15 to 20 is: " + interval)
+// console.log("Interval between 15 to 20 is: " + interval)
+// console.log("Interval between 15 to 20 is: " + interval)
+// console.log("Interval between 15 to 20 is: " + interval)
+// let sentence: string = "monika se uci javascript"
+// let word: string = "se"
+// if(sentence.includes("x")) {
+//     sentence = "neco jineho k jidlu"
+// }
+// console.log(sentence)
+// let ln: string = "novakova  "
+// let para: string ="ko"
+// let index: number = ln.indexOf(para)
+// console.log(ln.trim())
+// console.log(index)
+// let password: string = "a111"
+// let included: string = "1234"
+// let n1: number = 14
+// let n2: number = 8
+// function checkLenghtPass(): void {
+//     if(password.length >  n1) {
+//         console.log("Very good password")
+//     }
+//     else if(password.length >= n2 && password.length <=  n1) {
+//          console.log("good password")
+//     }
+//     else {
+//         console.log("Weak password")
+//     }
+// }
+// checkLenghtPass()
+// function checkedTruePassword(): void {
+//     if(password.includes(included)){
+//     console.log("Password is not allowed")
+// }
+// else {
+//     console.log("Password is right")
+// }
+// }
+// checkedTruePassword()
